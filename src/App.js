dda60aa1e1ce2510afde137482c12d2329e4548c
balazs4 <balazs4web@gmail.com>
2017-02-11 21:51:24 +0100

    add preact-mdl(modules?) and preact-router

--- src/App.js
+++ src/App.js
@@ -1,2 +1,22 @@
 import { h } from 'preact';
-export default () => (<div>My first preact app is coming soon!</div>)
+import { Router, Link } from 'preact-router';
+import 'material-design-lite';
+import mdl from 'preact-mdl/dist/preact-mdl'
+
+const Home = () => <h1>Home</h1>
+const GettingStarted = () => <h1>Getting started</h1>
+const About = ({id}) => <h1>About of {id}</h1>
+
+
+
+export default () => (
+    <div>
+        <Link href='/home'><mdl.Button>Home</mdl.Button></Link>
+        <Link href='/about'><mdl.Button>About</mdl.Button></Link>
+        <Router>
+            <Home path='/home' />
+            <About path='/about/:id?' />
+            <Home path='/' />
+        </Router>
+    </div>
+)
