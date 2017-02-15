85b239e828afb749ae37bd74709577b336e60ac4
balazs4 <balazs4web@gmail.com>
2017-02-15 23:30:13 +0100

    hey

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const {resolve} = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: resolve(__dirname, 'src', 'index.js'),
@@ -7,17 +9,15 @@ module.exports = {
         filename: 'webpack.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: resolve(__dirname, 'node_modules'),
-                use: {
-                    loader: 'buble-loader',
-                    options: {
-                        jsx: 'h'
-                    }
-                }
-            }
-        ]
-    }
+        rules: [{
+            test: /\.js$/,
+            include: resolve(__dirname, 'src'),
+            use: { loader: 'buble-loader', options: { jsx: 'h' } }
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') } }),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({ title: require('./package.json').name, template: resolve(__dirname, 'src', 'index.html') }),
+    ]
 }
