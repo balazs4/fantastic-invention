e444858bb4472a860cc64c01ba7aea4a9a781934
balazs4 <balazs4web@gmail.com>
2017-02-20 19:37:25 +0100

    welcome back, babel!

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-const { resolve } = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { name } = require("./package.json");
 
 module.exports = {
-  entry: resolve(__dirname, 'src', 'index.js'),
+  entry: resolve(__dirname, "src", "index.js"),
   output: {
-    path: resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: resolve(__dirname, "dist"),
+    filename: `${name}.js`
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: resolve(__dirname, 'src'),
-        use: 'babel-loader'
+        include: resolve(__dirname, "src"),
+        use: "babel-loader"
       }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development")
       }
     }),
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
-      title: require('./package.json').name,
-      template: resolve(__dirname, 'src', 'index.html')
+      title: name,
+      template: resolve(__dirname, "src", "index.html")
     })
   ]
 };
