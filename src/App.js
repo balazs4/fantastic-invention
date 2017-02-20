cc6c3a0e4b140395919f1b3ed93e90bfc7844221
balazs4 <balazs4web@gmail.com>
2017-02-20 22:17:04 +0100

    hmr + webpack-dev-server

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import 'material-design-lite';
 import { Button } from 'preact-mdl';
 
 const Home = () => <h1>Home</h1>;
-const About = ({ id }) => <h1>About of {id}</h1>;
+const About = ({ id = 'preact' }) => <h1>About {id}</h1>;
 
 export default () => (
   <div>
-    <Link href="/home"><Button>Home</Button></Link>
-    <Link href="/about"><Button>About</Button></Link>
+    <Link href="/home"><Button primary raised>Home</Button></Link>
+    <Link href="/about/preact"><Button accent>About</Button></Link>
     <Router>
       <Home path="/home" />
       <About path="/about/:id?" />
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const { resolve } = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { name } = require("./package.json");
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { name } = require('./package.json');
 
 module.exports = {
-  entry: resolve(__dirname, "src", "index.js"),
+  entry: resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: resolve(__dirname, "dist"),
+    path: resolve(__dirname, 'dist'),
     filename: `${name}.js`
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: resolve(__dirname, "src"),
-        use: "babel-loader"
+        include: resolve(__dirname, 'src'),
+        use: 'babel-loader'
       }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development")
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
       }
     }),
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       title: name,
-      template: resolve(__dirname, "src", "index.html")
+      template: resolve(__dirname, 'src', 'index.html')
     })
-  ]
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true,
+    compress: true
+  }
 };
